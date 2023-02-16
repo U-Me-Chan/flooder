5d22e7a0811cc63403587d9a6bd684b3a60389d6
kugimiya <bp.001@yandex.ru>
2023-02-16 13:09:47 +0400

    chore: Тюнинг чистки текста

--- lib/TextRefactor.ts
+++ lib/TextRefactor.ts
@@ -3,6 +3,8 @@ export class TextRefactor {
     return text.split('\n')
       .reduce((acc, cur) => ([...acc, ...cur.split('.')]), [] as string[])
       .map(str => str.trim())
+      .map(str => str.split('\t').join(' '))
+      .map(str => str.split(' ').filter(_ => _.length > 0).join(' '))
       .filter(str => Boolean(str))
       .filter(str => str.length > 10)
       .map(str => str.trim())
