687bb9023eb07c1c831faf99721640616bf7d7f2
kugimiya <bp.001@yandex.ru>
2023-02-19 02:15:49 +0400

    wip: черновик кравлера панорамы

---
+++ lib/crawlers/CrawlerPanorama.ts
@@ -0,0 +1,41 @@
+import { AbstractCrawler } from './AbstractCrawler';
+import { Storage } from '../Storage';
+
+const BASE_URL = 'https://panorama.pub';
+
+const CATEGORIES_DATE_PRESENTER = [
+  'politics',
+  'society',
+];
+
+const CATEGORIES_GALLERY_PRESENTER = [
+  'science',
+  'economics',
+];
+
+const CATEGORIES_ARTICLES_PRESENTER = [
+  'articles',
+];
+
+export class CrawlerPanorama implements AbstractCrawler {
+  breakTime = 5000;
+  name = 'panorama';
+  ready = false;
+  isRecallable = false;
+  storage: Storage;
+
+  constructor (storage: Storage) {
+    this.storage = storage;
+  }
+
+  async init(): Promise<void> {
+
+  }
+
+  async getNext(): Promise<{ text: string; nextAvailable: boolean; id?: string }> {
+    return {
+      text: '',
+      nextAvailable: false,
+    };
+  }
+}
